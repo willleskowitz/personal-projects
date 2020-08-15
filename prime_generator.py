6daaa4b3cdbc364e0f1113cfaaf2498dd8dc5b90
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-08-15 18:58:02 -0500

    Update prime_generator.py

--- prime_generator.py
+++ prime_generator.py
@@ -5,11 +5,32 @@ import math
 class Primes:
     @staticmethod
     def stream():
-        '''Prime generator. This method can generate a million primes in just 
-        under 25 seconds by utilizing the Miller-Rabin primality test while
-        skipping common composites. The k within the isprime function controls
-        the accuracy of the Miller-Rabin primality test. The test will declare
-        a composite as prime with a probability at most 4^−k. By default, k = 3.'''
+        '''My most recent attempt at creating a prime generator. This method
+        uses a sieve that grows as the more primes are generated and removes
+        unneeded entries to minimize memory use. This approach can generate
+        a million primes in about 5 seconds.'''
+        yield 2; yield 3; yield 5
+        sieve = {9: 3, 25: 5}
+        
+        c = 7
+        while True:
+            if c not in sieve:
+                sieve[c*c] = c; yield c
+            else:
+                p = sieve.pop(c)
+                n = c + 2*p
+                while n in sieve: n += 2*p
+                sieve[n] = p
+            c += 2    
+
+    @staticmethod
+    def stream2():
+        ''' My second attempt at creating a prime generator. This method can
+        generate a million primes in just under 25 seconds by utilizing the
+        Miller-Rabin primality test while skipping common composites. The k 
+        within the isprime function controls the accuracy of the Miller-Rabin 
+        primality test. The test will declare a composite as prime with a 
+        probability at most 4^−k. By default, k = 3.'''
         
         primes = [2, 3]
         
@@ -69,64 +90,11 @@ class Primes:
             last_n = n
             i += 1
             base = next_base
-    
-    
-    @staticmethod
-    def stream2():
-        '''Slightly less efficient prime generator. This method can generate a
-        million primes just under 33 seconds. This also utilizes the Miller-Rabin 
-        primality test but does not optimize by skipping common composites.'''
-        
-        yield 2
-        yield 3
-        
-        def miller(n, d):
-            a = random.randint(2, n - 1)
-            x = pow(a, d, n)
-            if x == 1 or x == n - 1: return True
-            
-            while d != n - 1:
-                x = (x*x) % n
-                d *= 2
-                
-                if x == 1: return False
-                if x == n - 1: return True
-                
-            return False
-            
-        
-        def isprime(n, k = 4):
-            
-            if n % 2 == 0 or n == 3: return False
-            
-            d = n - 1; 
-            while (d % 2 == 0): 
-                d //= 2
-                
-            for _ in range(k):
-                if miller(n, d) == False: return False
-            
-            return True
-        
-        n = 5
-        while n < 30:
-            if isprime(n):
-                yield n
-            n += 2
-            
-        k = 1
-        while True:
-            for i in (1, 7, 11, 13, 17, 19, 23, 29):
-                n = 30*k + i
-
-                if isprime(n):
-                    yield n
-            k += 1
             
     @staticmethod
     def stream3():
-        '''Inefficient prime generator, though all primes produced are actually 
-        primes, not probable primes.'''
+        '''My first, and least efficient, attempt at creating an
+        infinite prime generator.'''
         primes = [2, 3]
 
         yield 2
@@ -158,15 +126,3 @@ class Primes:
                     primes.append(n)
                     yield n
             k += 1
-            
-    @staticmethod
-    def sieve_of_eratosthenes(max_n):
-        '''Implementation of the ancient Sieve of Eratosthenes, which will return all
-        prime numbers up to max_n as a list.'''
-        primes = [2] + list(range(3, max_n + 1, 2))
-        i = 1
-        while primes[i] < primes[-1]**0.5:
-            [primes.remove(n) for n in range(primes[i]*2, primes[-1] + 1, primes[i]) if n in primes]
-            i += 1
-        
-        return primes

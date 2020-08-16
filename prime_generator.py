b5d0be683f0deaf7984ac38c846cdd1660a6bbda
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-08-15 23:22:49 -0500

    Update prime_generator.py

--- prime_generator.py
+++ prime_generator.py
@@ -6,24 +6,48 @@ from itertools import count, cycle, compress
 class Primes:
     @staticmethod
     def stream_v4():
-        '''My most efficient prime generator. This method uses a sieve that
+        '''Efficient prime generator. This method uses a sieve that
         grows as the more primes are generated and removes unneeded entries
         to minimize memory use. This approach can generate a million primes
-        in around 3.2 seconds.'''
+        in around 3.2 seconds on my machine.
+        
+        
+        This function is based off a submission by user tzot from StackOverflow,
+        with some minor changes. First, mod was a hardcoded frozenset of integers. Albeit
+        this is more efficient, but it fails to explain the rationale behind it.
+        This part of the code is exploiting the fact that all primes above 30
+        (2*3*5) can be written in the form 30*k + i where k is a natural number 
+        and 1<i<30 such that gcd(i,30) = 1. Representing primes this way increases
+        generation efficiency by avoiding multiples of 2, 3, and 5.
+        
+        Second, skip was a hardcoded tuple of fifteen 1's and 0's for the cycle function.
+        Instead, I included a list comprehension representing the logic of this 
+        variable. For a prime p where p >= 7, p % 30 = i such that gcd(i,30) = 1. 
+        This becomes evident when reviewing the representation p = 30*k + i.
+        Therefore, any numbers not in this format can be skipped. Hence, skip
+        represents this in a format thecycle function can read to filter these
+        cases. This can be cycled since i remains the same as k increases.
+        
+        Lastly, the entries {9: 3, 25: 5} were removed from the sieve as they would
+        never be use since the filter skips multiples of 3's and 5's as mentioned above.
+        
+        Any other changes made were for readability. I hope you find this useful!'''
 
         yield 2; yield 3; yield 5
         sieve = {}
-        mod = [i for i in range(30) if math.gcd(i, 30) == 1]
-        skip = tuple([1 if n % 30 in mod else 0 for n in range(7, 37, 2)])
+        start = 7
+        base = 2*3*5
+        mod = [i for i in range(base) if math.gcd(i, base) == 1]
+        skip = tuple([1 if n % base in mod else 0 for n in range(start, base + start, 2)])
         mod = frozenset(mod)
 
-        for c in compress(count(7, 2), cycle(skip)):
+        for c in compress(count(start, 2), cycle(skip)):
             if c not in sieve:
                 sieve[c*c] = c; yield c
             else:
                 p = sieve.pop(c)
                 n = c + 2*p
-                while n in sieve or n % 30 not in mod: n += 2*p
+                while n in sieve or n % base not in mod: n += 2*p
                 sieve[n] = p
     
     @staticmethod
@@ -32,8 +56,7 @@ class Primes:
         with base 210 (2*3*5*7) rather than base 30 (2*3*5). My hunch was that
         this approach would be an improvement since multiples of 7 would be 
         skipped, though I was wrong. I included the function mostly because
-        of its novelty, and to preserve the mathematics. It can produce a
-        million primes in 6.38 seconds.''' 
+        of its novelty. It can produce a million primes in 6.38 seconds.''' 
         
         start = 31; base = 210
         initial_primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29)

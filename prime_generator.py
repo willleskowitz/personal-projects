1f01be5f2b3b6d42d09776140cfdce4f8a8271bf
Will Leskowitz <59721872+willleskowitz@users.noreply.github.com>
2020-08-15 20:49:37 -0500

    Update prime_generator.py

--- prime_generator.py
+++ prime_generator.py
@@ -1,32 +1,62 @@
 import random
 import math
+from itertools import count, cycle, compress
 
 
 class Primes:
     @staticmethod
-    def stream():
-        '''My most recent attempt at creating a prime generator. This method
-        uses a sieve that grows as the more primes are generated and removes
-        unneeded entries to minimize memory use. This approach can generate
-        a million primes in about 5 seconds.'''
+    def stream_v4():
+        '''My most efficient prime generator. This method uses a sieve that
+        grows as the more primes are generated and removes unneeded entries
+        to minimize memory use. This approach can generate a million primes
+        in just over 3.74 seconds.'''
+
         yield 2; yield 3; yield 5
-        sieve = {9: 3, 25: 5}
-        
-        c = 7
-        while True:
+        sieve = {}
+        mod = tuple([i for i in range(30) if math.gcd(i, 30) == 1])
+        skip = tuple([1 if n % 30 in mod else 0 for n in range(7, 37, 2)])
+
+        for c in compress(count(7, 2), cycle(skip)):
             if c not in sieve:
                 sieve[c*c] = c; yield c
             else:
                 p = sieve.pop(c)
                 n = c + 2*p
-                while n in sieve: n += 2*p
+                while n in sieve or n % 30 not in mod: n += 2*p
                 sieve[n] = p
-            c += 2    
+    
+    @staticmethod
+    def stream_v3():
+        '''This prime generator uses a similar algorithm as stream_v4, though
+        with base 210 (2*3*5*7) rather than base 30 (2*3*5). My hunch was that
+        this approach would be an improvement since multiples of 7 would be 
+        skipped, though I was wrong. I included the function mostly because
+        of its novelty, and to preserve the mathematics. It can produce a
+        million primes in 6.38 seconds.''' 
+        
+        start = 31; base = 210
+        initial_primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
+        for prime in initial_primes:
+            yield prime
+
+        sieve = {49: 7, 121: 11, 169: 13, 289: 17, 361: 19, 529: 23, 841: 29, 961: 31}
+
+        mod = [i for i in range(base) if math.gcd(i, base) == 1]
+        skip = [1 if n % base in mod else 0 for n in range(start, start + base, 2)]
+
+        for c in compress(count(start, 2), cycle(skip)):
+            if c not in sieve:
+                sieve[c*c] = c; yield c
+            else:
+                p = sieve.pop(c)
+                n = c + 2*p
+                while n in sieve or n % base not in mod: n += 2*p
+                sieve[n] = p
 
     @staticmethod
-    def stream2():
+    def stream_v2():
         ''' My second attempt at creating a prime generator. This method can
-        generate a million primes in just under 25 seconds by utilizing the
+        generate a million primes in ~25 seconds by utilizing the
         Miller-Rabin primality test while skipping common composites. The k 
         within the isprime function controls the accuracy of the Miller-Rabin 
         primality test. The test will declare a composite as prime with a 
@@ -92,7 +122,7 @@ class Primes:
             base = next_base
             
     @staticmethod
-    def stream3():
+    def stream_v1():
         '''My first, and least efficient, attempt at creating an
         infinite prime generator.'''
         primes = [2, 3]
